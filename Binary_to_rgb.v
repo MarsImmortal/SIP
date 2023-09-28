01eb9b2040f1038a83e30652206c27fa31df8555
MarsImmortal <s222198007@deakin.edu.au>
2023-09-28 11:14:35 +1000

    refactor : simplify and improve binary to png

--- Binary_to_rgb.v
+++ Binary_to_rgb.v
@@ -3,14 +3,14 @@
 module BinaryToPNG (
   input wire clk,
   input wire rst,
-  input wire [7:0] binary_image_pixel, // Assuming 8-bit grayscale image
+  input wire [7:0] binary_image_pixel,
   output wire [7:0] png_pixel_r,
   output wire [7:0] png_pixel_g,
   output wire [7:0] png_pixel_b,
   output wire png_pixel_valid
 );
 
-  reg [7:0] image_buffer [0:255][0:255]; // Assuming a 256x256 image size
+  reg [7:0] image_buffer [0:255][0:255];
   reg [7:0] current_pixel_r;
   reg [7:0] current_pixel_g;
   reg [7:0] current_pixel_b;
@@ -20,7 +20,6 @@ module BinaryToPNG (
 
   always @(posedge clk or posedge rst) begin
     if (rst) begin
-      // Reset logic here
       row_counter <= 8'b0;
       col_counter <= 8'b0;
       current_pixel_r <= 8'b0;
@@ -28,34 +27,25 @@ module BinaryToPNG (
       current_pixel_b <= 8'b0;
       pixel_valid <= 0;
     end else begin
-      // Image processing logic here
       if (row_counter < 256 && col_counter < 256) begin
-        // Store binary image pixel in the buffer
         image_buffer[row_counter][col_counter] <= binary_image_pixel;
-
-        // Generate RGB values for the PNG pixel (for simplicity, copying binary pixel to each channel)
         current_pixel_r <= binary_image_pixel;
         current_pixel_g <= binary_image_pixel;
         current_pixel_b <= binary_image_pixel;
-
-        // Output pixel is valid
         pixel_valid <= 1;
       end else begin
-        // Output pixel is invalid when the image is processed
         pixel_valid <= 0;
-      end
+      end;
 
-      // Update counters for the next pixel
       if (col_counter < 255) begin
         col_counter <= col_counter + 1;
       end else if (row_counter < 255) begin
         col_counter <= 0;
         row_counter <= row_counter + 1;
-      end
-    end
+      end;
+    end;
   end
 
-  // Output signals
   assign png_pixel_r = current_pixel_r;
   assign png_pixel_g = current_pixel_g;
   assign png_pixel_b = current_pixel_b;
