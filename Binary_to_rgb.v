0041f165b81b60479d992fb5fe379b2d73a8ff46
MarsImmortal <s222198007@deakin.edu.au>
2023-10-06 00:56:48 +1100

    enhanced the code to improve fuctionality and add a 2d array to store the pixel values

--- Binary_to_rgb.v
+++ Binary_to_rgb.v
@@ -7,16 +7,16 @@ module BinaryToRGB (
   output wire [7:0] rgb_pixel_r,
   output wire [7:0] rgb_pixel_g,
   output wire [7:0] rgb_pixel_b,
-  output wire rgb_pixel_valid
+  output wire rgb_pixel_valid,
+  output reg [7:0] rgb_pixel_array [0:WIDTH-1][0:HEIGHT-1]
 );
 
-  localparam WIDTH = 256;
-  localparam HEIGHT = 256;
+  localparam WIDTH = 10;
+  localparam HEIGHT = 10;
 
-  // Declare image buffer as reg [7:0] type
+
   reg [7:0] image_buffer [0:WIDTH-1][0:HEIGHT-1];
 
-  // Declare registers for the current pixel and counters
   reg [7:0] current_pixel_r;
   reg [7:0] current_pixel_g;
   reg [7:0] current_pixel_b;
@@ -26,7 +26,6 @@ module BinaryToRGB (
 
   always @(posedge clk or posedge rst) begin
     if (rst) begin
-      // Reset all registers and counters
       current_pixel_r <= 8'b0;
       current_pixel_g <= 8'b0;
       current_pixel_b <= 8'b0;
@@ -35,15 +34,18 @@ module BinaryToRGB (
       pixel_valid <= 0;
     end else begin
       if (row_counter < WIDTH && col_counter < HEIGHT) begin
-        // Store binary_image_pixel in image_buffer
+
         image_buffer[row_counter][col_counter] <= binary_image_pixel;
-        // Set RGB components from binary_image_pixel
+
         current_pixel_r <= binary_image_pixel;
         current_pixel_g <= binary_image_pixel;
         current_pixel_b <= binary_image_pixel;
         pixel_valid <= 1;
+        
+
+        rgb_pixel_array[row_counter][col_counter] <= {current_pixel_r, current_pixel_g, current_pixel_b};
       end else begin
-        // Set pixel_valid to 0 when image processing is complete
+
         pixel_valid <= 0;
       end;
 
@@ -56,10 +58,13 @@ module BinaryToRGB (
     end;
   end
 
-  // Output assignments for RGB components and valid flag
+
   assign rgb_pixel_r = current_pixel_r;
   assign rgb_pixel_g = current_pixel_g;
   assign rgb_pixel_b = current_pixel_b;
   assign rgb_pixel_valid = pixel_valid;
 
+
 endmodule
+
+
