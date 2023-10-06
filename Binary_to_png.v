6f72323568807cabd35f79b415ea15e85fb853bb
ManavSingh003 <106513689+ManavSingh003@users.noreply.github.com>
2023-10-07 00:08:27 +1100

    Update Binary_to_png.v

--- Binary_to_png.v
+++ Binary_to_png.v
@@ -1,10 +1,13 @@
+`timescale 1ns/1ns
+
 module BinaryToPNG (
   input wire clk,
   input wire rst,
   input wire [7:0] binary_image_pixel,
   output wire [7:0] png_pixel_r,
   output wire [7:0] png_pixel_g,
-  output wire [7:0] png_pixel_b
+  output wire [7:0] png_pixel_b,
+  output wire png_pixel_valid
 );
 
   reg [7:0] image_buffer [0:255][0:255];
@@ -13,6 +16,7 @@ module BinaryToPNG (
   reg [7:0] current_pixel_b;
   reg [7:0] row_counter;
   reg [7:0] col_counter;
+  reg pixel_valid;
 
   always @(posedge clk or posedge rst) begin
     if (rst) begin
@@ -21,15 +25,19 @@ module BinaryToPNG (
       current_pixel_r <= 8'b0;
       current_pixel_g <= 8'b0;
       current_pixel_b <= 8'b0;
+      pixel_valid <= 0;
     end else begin
       if (row_counter < 256 && col_counter < 256) begin
         image_buffer[row_counter][col_counter] <= binary_image_pixel;
         current_pixel_r <= binary_image_pixel;
         current_pixel_g <= binary_image_pixel;
         current_pixel_b <= binary_image_pixel;
+        pixel_valid <= 1;
+      end else begin
+        pixel_valid <= 0;
       end;
 
-      if (col_counter < 255) begin
+       if (col_counter < 255) begin
         col_counter <= col_counter + 1;
       end else if (row_counter < 255) begin
         col_counter <= 0;
@@ -41,5 +49,6 @@ module BinaryToPNG (
   assign png_pixel_r = current_pixel_r;
   assign png_pixel_g = current_pixel_g;
   assign png_pixel_b = current_pixel_b;
+  assign png_pixel_valid = pixel_valid;
 
 endmodule
